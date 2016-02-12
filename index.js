f02250d703e4ec1fcaeca3fca0292ef29a44f505
Nick Bottomley <nhbottomley@gmail.com>
2016-02-11 21:47:35 -0800

    inline creating request Sets

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 "use strict";
 
-const getWork = () => new Set(process._getActiveRequests());
-
 function monitorActiveRequests (handler) {
 
   const tag = h => h.__start = Date.now();
 
-  let timer = setImmediate(monitor, getWork(), handler);
+  let timer = setImmediate(monitor, new Set(process._getActiveRequests()), handler);
 
   function monitor (prev, fn) {
-    const current = getWork();
+    const current = new Set(process._getActiveRequests());
     const removed = Array.from(prev).filter(x => !current.has(x));
     const added   = Array.from(current).filter(x => !prev.has(x));
     added.forEach(tag);
